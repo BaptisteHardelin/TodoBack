9bf611b274ca45f771f17570a29a7b0469fb6c31
Baptiste Hardelin <91209529+BaptisteHardelin@users.noreply.github.com>
2023-02-19 15:09:23 +0100

    Change postgres package to the good one + connection to the db

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,7 +1,24 @@
+require("dotenv").config();
+const { Pool, Client } = require("pg");
+
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
+
+const pool = new Pool({
+  connectionString,
+});
+
+const helloDB = (req, result) => {
+  pool.query("SELECT * from todo", (err, res) => {
+    result.json(res.rows);
+  });
+  pool.end();
+};
+
 const helloWorld = (req, res) => {
   res.json({ hello: "Hello World" });
 };
 
 module.exports = {
   helloWorld,
+  helloDB,
 };
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const routes = express.Router();
-const { helloWorld } = require("../controller/todoController");
+const { helloWorld, helloDB } = require("../controller/todoController");
 
 routes.get("/", helloWorld);
+routes.get("/db", helloDB);
 
 module.exports = routes;
